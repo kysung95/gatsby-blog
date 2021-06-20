df5bd7ee61a2430bd8c2f8b2a912b885e929f0ab
kysung95 <yoyo95@kyonggi.ac.kr>
2021-06-21 00:14:03 +0900

    [UPDATE] 1, add CategoryList 2, add CategoryItem

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,14 @@ import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
 import GlobalStyle from 'components/Common/GlobalStyle';
 import Introduction from 'components/Main/Introduction';
+import CategoryList from 'components/Main/CategoryList';
+import Footer from 'components/Common/Footer';
+
+const CATEGORY_LIST = {
+    All: 5,
+    Web: 3,
+    Mobile: 2,
+  };
 
 const Container = styled.div`
   display: flex;
@@ -14,7 +22,10 @@ const IndexPage: FunctionComponent = function () {
     <Container>
       <GlobalStyle />
       <Introduction />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
+      <Footer/>  
     </Container>
+    
   );
 };
 
