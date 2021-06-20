810a0c5138393f571772745f32a72a99aa618daf
kysung95 <yoyo95@kyonggi.ac.kr>
2021-06-21 00:34:14 +0900

    [UPDATE] 1,adjust responsive style

--- src/components/Main/CategoryList.tsx
+++ src/components/Main/CategoryList.tsx
@@ -24,10 +24,16 @@ const CategoryListWrapper = styled.div`
   flex-wrap: wrap;
   width: 768px;
   margin: 100px auto 0;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-top: 50px;
+    padding: 0 20px;
+  }
 `;
 
-const CategoryItem = styled(({ active, to, ...props }: GatsbyLinkProps) => (
-    <Link to={to} {...props} />
+const CategoryItem = styled(({ active, ...props }: GatsbyLinkProps) => (
+    <Link {...props} />
   ))<CategoryItemProps>`
     margin-right: 20px;
     padding: 5px 0;
@@ -37,6 +43,10 @@ const CategoryItem = styled(({ active, to, ...props }: GatsbyLinkProps) => (
   
     &:last-of-type {
       margin-right: 0;
+    }
+  
+    @media (max-width: 768px) {
+      font-size: 15px;
     }
   `;
 
--- src/components/Main/Introduction.tsx
+++ src/components/Main/Introduction.tsx
@@ -12,28 +12,44 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
-  width: 100%;
+  align-items: flex-start;
+  width: 768px;
   height: 400px;
   margin: 0 auto;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 300px;
+    padding: 0 20px;
+  }
 `;
 
 const SubTitle = styled.div`
   font-size: 20px;
   font-weight: 400;
+
+  @media (max-width: 768px) {
+    font-size: 15px;
+  }
 `;
 
 const Title = styled.div`
   margin-top: 5px;
   font-size: 35px;
   font-weight: 700;
+
+  @media (max-width: 768px) {
+    font-size: 25px;
+  }
 `;
+
 
 const Introduction: FunctionComponent = function () {
   return (
     <Background>
       <Wrapper>
         <ProfileImage />
+
         <div>
           <SubTitle>Nice to Meet You,</SubTitle>
           <Title>I'm Junior Frontend Developer YongSeong.</Title>
