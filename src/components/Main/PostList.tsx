0bfb6304127772f0be1716c3c3b6d45f55e678d8
kysung95 <yoyo95@kyonggi.ac.kr>
2021-06-21 14:17:25 +0900

    [UPDATE] 1,add post thumbnail 2,change some props

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -2,15 +2,24 @@ import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
 import PostItem from 'components/Main/PostItem';
 
-const POST_ITEM_DATA = {
-  title: 'Post Item Title',
-  date: '2021.06.21.',
-  categories: ['Web', 'Frontend', 'Testing'],
-  summary:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
-  thumbnail: 'https://t1.daumcdn.net/cfile/tistory/991EDA445EA7C3090A',
-  link: 'https://www.google.co.kr/',
+export type PostType = {
+  node: {
+    id: string;
+    frontmatter: {
+      title: string;
+      summary: string;
+      date: string;
+      categories: string[];
+      thumbnail: {
+        publicURL: string;
+      };
+    };
+  };
 };
+
+interface PostListProps {
+  posts: PostType[];
+}
 
 const PostListWrapper = styled.div`
   display: grid;
@@ -27,13 +36,27 @@ const PostListWrapper = styled.div`
   }
 `;
 
-const PostList: FunctionComponent = function () {
+const PostList: FunctionComponent<PostListProps> = function ({ posts }) {
   return (
     <PostListWrapper>
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
+      {posts.map(
+        ({
+          node: {
+            id,
+            frontmatter: {
+              thumbnail: { publicURL },
+              ...rest
+            },
+          },
+        }: PostType) => (
+          <PostItem
+            {...rest}
+            thumbnail={publicURL}
+            link="<https://www.google.co.kr/>"
+            key={id}
+          />
+        ),
+      )}
     </PostListWrapper>
   );
 };
